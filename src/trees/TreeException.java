41141b34476b5a005981453f8405b8aa5e21452d
Shij03 <justinshi00@gmail.com>
2023-11-17 08:47:48 -0800

    initial checkin

---
+++ src/trees/TreeException.java
@@ -0,0 +1,12 @@
+package trees;
+
+import java.io.IOException;
+
+public class TreeException extends IOException
+{
+	public TreeException(String s)
+	{
+		super(s);
+		// Pass s into a superclass ctor.
+	}
+}
